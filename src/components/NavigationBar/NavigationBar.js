e3e6135ddc6b57b7e44960c8dc0d3d5d5e7f052e
shuyanglin <shuyanglin.tw@gmail.com>
2023-04-11 14:49:09 +0100

    Setup navigation bar, and responsiveness

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,20 +1,35 @@
 import React from "react";
-
+import {FaTimes, FaBars} from 'react-icons/fa'
 import styles from './NavigationBar.module.css';
+import "../../Styles/main.css"
 
 function Header() {
+
+  const [isResponsive, setIsResponsive] = React.useState();
+
+  const navStyle = "responsiveNav";
+  function showNavbar(){
+    setIsResponsive(!isResponsive);
+  }
+
   return (
     <header>
-      <h3>Shu Yang Lin</h3>
-      <nav>
+      <div>
+        <h3>Shu Yang Lin</h3>
+        <p>re:architect</p>
+      </div>
+      <nav className={isResponsive?navStyle:undefined}>
         <a href="/#">About</a>
         <a href="/#">Projects</a>
         <a href="/#">Weekly Notes</a>
-        <a href="/#">Talks</a>
-        <a href="/#">Workshops</a>
         <a href="/#">Calendar</a>
-        <button></button>
+        <button className={`${styles.navBtn} ${styles.navCloseBtn}`} onClick={showNavbar}>
+          <FaTimes/>
+        </button>
       </nav>
+      <button className={styles.navBtn} onClick={showNavbar}>
+        <FaBars/>
+      </button>
     </header>
   );
 }
