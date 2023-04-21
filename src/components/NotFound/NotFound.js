80155fb85fd95a41bd160af23faf4f30e91666dc
shuyanglin <shuyanglin.tw@gmail.com>
2023-04-21 11:43:51 +0100

    use <HashRouter> because github pages doesn't suport the html5 history api which is needed for <BrowserRouter>. This hopefully fixes #5.

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -7,9 +7,7 @@ function NotFound() {
     <Container>
       <Row>
         <Col lg={4} sm={12}>
-          
           <img alt="shu's memoji for 404 page" src={imgSrc}></img>
-    
         </Col>
         <Col lg={8} sm={12} style={{margin: 'auto'}}>
         <h1>404 Not Found</h1>
