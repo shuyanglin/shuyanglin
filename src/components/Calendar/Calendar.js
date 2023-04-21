50b0c98b9b1e9f963e1c65fc76f16eacf09e6110
shuyanglin <shuyanglin.tw@gmail.com>
2023-04-21 16:45:19 +0100

    add past events

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -47,7 +47,105 @@ function Calendar() {
     topic: 'Democracy in Taiwan: The People-Public-Private Partnership',
     organisation: [{name: 'Köerber Stiftung Foundation', link: "https://koerber-stiftung.de/"}],
     location: 'Hamburg, Germany'
+  },{
+    date: '2021.1.13',
+    type: 'Presentation',
+    topic: 'CIID open lecture 2021',
+    organisation: [{name: 'Copenhagen Institute of Interaction Design (CIID)', link: "https://ciid.dk"}],
+    location: 'Online'
+  },{
+    date: '2020.6.17',
+    type: 'Presentation',
+    topic: 'Digital Lightning Talks',
+    organisation: [{name: 'Copenhagen Institute of Interaction Design (CIID)', link: "https://ciid.dk"}],
+    location: 'Online'
+  },{
+    date: '2020.5.25-6.16',
+    type: 'Workshop',
+    topic: 'Managing a gender responsive ADMS',
+    organisation: [{name: 'Digital Future Society (DFS)', link: "https://digitalfuturesociety.com/"}],
+    location: 'Online'
+  },{
+    date: '2019.11.6',
+    type: 'Presentation',
+    topic: 'Prototying Future Democracy ',
+    organisation: [{name: 'FWD50', link: "https://2021.fwd50.com/"}],
+    location: 'Toronto, Canada'
+  },{
+    date: '2019.9.16-17',
+    type: 'Presentation',
+    topic: 'EmpoderaLIVE ',
+    organisation: [{name: 'Fundación Cibervoluntarios', link: "www.cibervoluntarios.org"}],
+    location: 'Malaga, Spain'
+  },{
+    date: '2019.6.3',
+    type: 'Presentation',
+    topic: 'Democratic innovations in Taiwan',
+    organisation: [{name: 'Democracy Accelerator', link: "https://www.democraticinnovations.com/about/"}],
+    location: ' Helsinki, Finland'
+  },{
+    date: '2018.11.5-17',
+    type: 'Hackathon',
+    topic: 'Collective Intelligence for Democracy',
+    organisation: [{name: 'Medialab-prado', link: "https://medialab-prado.es"}],
+    location: 'Madrid, Spain'
+  },{
+    date: '2018.10.29-30',
+    type: 'Presentation',
+    topic: 'Renovation of Innovation',
+    organisation: [{name: 'Future Innovation Forum 2018', link: "http://www.seoulfif.co.kr/en/"},{name: 'Seoul Metropolitan Government', link: "https://english.seoul.go.kr"}],
+    location: 'Seoul, Korea'
+  },{
+    date: '2018.9.4-5',
+    type: 'Presentation',
+    topic: 'Broken Promise Land',
+    organisation: [{name: 'The Conference', link: "https://2018.theconference.se/"}],
+    location: 'Malmö, Sweden'
+  },{
+    date: '2018.8.29',
+    type: 'Presentation',
+    topic: 'Prototying The Future Of Democracy',
+    organisation: [{name: 'Above All Humans (AAH)', link: "https://www.aboveallhuman.co"}],
+    location: 'Melbourne, Australia'
+  },{
+    date: '2018.7.26-27',
+    type: 'Presentation',
+    topic: 'Open Co-op conference',
+    organisation: [{name: 'Open Cooperatives', link: "https://2018.open.coop"}],
+    location: 'London, UK'
+  },{
+    date: '2018.7.17-18',
+    type: 'Presentation',
+    topic: 'International Design in Government Conference',
+    organisation: [{name: 'International Design in Government', link: "https://international.gov-design.com/"}],
+    location: 'London, UK'
+  },{
+    date: '2018.7.16',
+    type: 'Workshop',
+    topic: 'OneTeamGov Global Conference',
+    organisation: [{name: 'OneTeamGov', link: "https://www.oneteamgov.uk"}],
+    location: 'London, UK'
+  },{
+    date: '2018.6.7-8',
+    type: 'Workshop',
+    topic: 'PO, vTaiwan & Holopolis',
+    organisation: [{name: 'Personal Democracy Forum', link: "https://www.youtube.com/@personaldemocracy"}],
+    location: 'New York, UK'
+  },{
+    date: '2018.5.12',
+    type: 'Presentation',
+    topic: 'A Call for Collaboration',
+    organisation: [{name: 'TEDxNCTU', link: "https://www.ted.com/tedx/events/10477"}],
+    location: 'Hsinchu, Taiwan'
+  },{
+    date: '2017.9.2-8',
+    type: 'Presentation',
+    topic: 'Our Urban Future',
+    organisation: [{name: 'Grinnell College', link: "https://www.grinnell.edu"}],
+    location: 'Grinnell, Iowa, US'
   }];
+
+
 
   console.log(commitment);
   
